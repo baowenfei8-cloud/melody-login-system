f0aad4e4928be33d1bd233c51a8b7020a6c880a1
baowenfei8-cloud <baowenfei8@gmail.com>
2026-07-24 19:35:30 +0800

    Add src/main/java/com/musiclogin/MusicLoginApplication.java

---
+++ src/main/java/com/musiclogin/MusicLoginApplication.java
@@ -0,0 +1,12 @@
+package com.musiclogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicLoginApplication.class, args);
+    }
+}
