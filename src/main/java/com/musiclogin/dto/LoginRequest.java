dae47e3ffa07b456b6e0538eac819d3edc11142a
baowenfei8-cloud <baowenfei8@gmail.com>
2026-07-24 19:35:44 +0800

    Add src/main/java/com/musiclogin/dto/LoginRequest.java

---
+++ src/main/java/com/musiclogin/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.musiclogin.dto;
+
+/**
+ * 登录请求数据传输对象
+ */
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
